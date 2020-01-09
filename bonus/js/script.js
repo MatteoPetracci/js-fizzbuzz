4cf5ca6b909aabde8d3d95982596ddab97a7164b
Matteo <matteop18@gmail.com>
2020-01-09 16:24:14 +0100

    Pulizia codice 2 metodo ciclo while

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -2,45 +2,47 @@ var i = 1;
 
 // METODO 1
 
-
-while (i < 101) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log('FizzBuzz');
-    document.getElementById('lista').innerHTML += "<li>" + "FizzBuzz" + "</li>";
-  }
-  if (i % 3 == 0) {
-    console.log('Fizz');
-    document.getElementById('lista').innerHTML += "<li>" + "Fizz" + "</li>";
-  } else if (i % 5 == 0) {
-    console.log('Buzz');
-    document.getElementById('lista').innerHTML += "<li>" + "Buzz" + "</li>";
-  } else {
-    console.log(i);
-    document.getElementById('lista').innerHTML += "<li>" + i + "</li>";
-  }
-  i++;
-}
-
-// METODO 2
 //
-// var testo;
 // while (i < 101) {
 //   if (i % 3 == 0 && i % 5 == 0) {
 //     console.log('FizzBuzz');
-//     testo = "Fizz";
 //     document.getElementById('lista').innerHTML += "<li>" + "FizzBuzz" + "</li>";
 //   }
 //   if (i % 3 == 0) {
 //     console.log('Fizz');
-//     testo = "Fizz";
 //     document.getElementById('lista').innerHTML += "<li>" + "Fizz" + "</li>";
 //   } else if (i % 5 == 0) {
 //     console.log('Buzz');
-//     testo = "Buzz";
 //     document.getElementById('lista').innerHTML += "<li>" + "Buzz" + "</li>";
 //   } else {
-//     testo = i;
-//     document.getElementById('lista').innerHTML += "<li>" + testo + "</li>";
+//     console.log(i);
+//     document.getElementById('lista').innerHTML += "<li>" + i + "</li>";
 //   }
 //   i++;
 // }
+
+// METODO 2
+
+var testo;
+while (i < 101) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz');
+    testo = "Fizz";
+    // document.getElementById('lista').innerHTML += "<li>" + "FizzBuzz" + "</li>";
+  }
+  if (i % 3 == 0) {
+    console.log('Fizz');
+    testo = "Fizz";
+    // document.getElementById('lista').innerHTML += "<li>" + "Fizz" + "</li>";
+  } else if (i % 5 == 0) {
+    console.log('Buzz');
+    testo = "Buzz";
+    // document.getElementById('lista').innerHTML += "<li>" + "Buzz" + "</li>";
+  } else {
+    testo = i;
+    // document.getElementById('lista').innerHTML += "<li>" + testo + "</li>";
+  }
+  document.getElementById('lista').innerHTML += "<li>" + testo + "</li>";
+
+  i++;
+}
