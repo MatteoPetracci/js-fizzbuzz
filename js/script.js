75d119d4cacb5c8dd0c00c323e4e0b87099c0268
Matteo <matteop18@gmail.com>
2020-01-09 14:47:04 +0100

    primo commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Fizz Buzz test:
+// Scrivi un programma che stampi i numeri da 1
+// a 100,
+// ma per i multipli di 3 stampi “Fizz” al
+// posto del numero e per i multipli di 5 stampi
+// Buzz.
+// Per i numeri che sono sia multipli di 3
+// che di 5 stampi FizzBuzz.
+// Stampiamo come sempre prima in console e poi su html
+// Bonus: usare due tipi di cicli, se usate per il primo il for in bonus  fate un while.
+
+
+
+var num = 0;
+
+for (var i = 0; i < 101; i++) {
+  // console.log(num + i);
+  var somma = num + i;
+
+  if (somma % 3 == 0) {
+    console.log('Fizz');
+  } else if (somma % 5 == 0) {
+    console.log('Buzz');
+  } else {
+    console.log(somma);
+  }
+  if (somma % 3 == 0 && somma % 5 == 0) {
+    console.log('FizzBuzz');
+  }
+}
