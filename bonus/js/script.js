b9ce6ee1481007675e8da18367e61c63a62ca6e0
Matteo <matteop18@gmail.com>
2020-01-09 16:25:47 +0100

    finito

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -28,21 +28,16 @@ while (i < 101) {
   if (i % 3 == 0 && i % 5 == 0) {
     console.log('FizzBuzz');
     testo = "Fizz";
-    // document.getElementById('lista').innerHTML += "<li>" + "FizzBuzz" + "</li>";
   }
   if (i % 3 == 0) {
     console.log('Fizz');
     testo = "Fizz";
-    // document.getElementById('lista').innerHTML += "<li>" + "Fizz" + "</li>";
   } else if (i % 5 == 0) {
     console.log('Buzz');
     testo = "Buzz";
-    // document.getElementById('lista').innerHTML += "<li>" + "Buzz" + "</li>";
   } else {
     testo = i;
-    // document.getElementById('lista').innerHTML += "<li>" + testo + "</li>";
   }
   document.getElementById('lista').innerHTML += "<li>" + testo + "</li>";
-
   i++;
 }
